0e1aae5af3fd71df84f17239cf3abe8c41f58470
Hee-jin506 <gmlwls35220@gmail.com>
2021-07-02 18:03:02 +0900

    로그인

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,31 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassord, this.password, function(err, isMatch) {
+        if (err) return cb(err),
+        cb(null, isMatch)
+    })
+
+}
 
 const User = mongoose.model('User', userSchema)
 
