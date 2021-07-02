67c369dbf17a5db34f5c96f1432c374403eb49d5
Hee-jin506 <gmlwls35220@gmail.com>
2021-07-02 17:07:38 +0900

    회원가입 기능

--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
-
+const config = require('./config/key');
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
-mongoose.connect('', {
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+.catch(err => console.log(err));
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
 })
 
 app.listen(port, () => {
